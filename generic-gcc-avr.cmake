bc0c1f0850e80535527b4c2828cffce8dfbabf62
Markus Siemens <siemens1993@gmail.com>
2015-10-26 15:14:49 +0100

    Update generic-gcc-avr.cmake

--- generic-gcc-avr.cmake
+++ generic-gcc-avr.cmake
@@ -38,6 +38,7 @@ option(WITH_MCU "Add the mCU type to the target file name." ON)
 ##########################################################################
 find_program(AVR_CC avr-gcc)
 find_program(AVR_CXX avr-g++)
+find_program(AVR_AS avr-as)
 find_program(AVR_OBJCOPY avr-objcopy)
 find_program(AVR_SIZE_TOOL avr-size)
 find_program(AVR_OBJDUMP avr-objdump)
@@ -49,6 +50,7 @@ set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_PROCESSOR avr)
 set(CMAKE_C_COMPILER ${AVR_CC})
 set(CMAKE_CXX_COMPILER ${AVR_CXX})
+set(CMAKE_ASM_COMPILER ${AVR_AS})
 
 ##########################################################################
 # some necessary tools and variables for AVR builds, which may not
@@ -154,7 +156,6 @@ function(add_avr_executable EXECUTABLE_NAME)
    set(elf_file ${EXECUTABLE_NAME}${MCU_TYPE_FOR_FILENAME}.elf)
    set(hex_file ${EXECUTABLE_NAME}${MCU_TYPE_FOR_FILENAME}.hex)
    set(map_file ${EXECUTABLE_NAME}${MCU_TYPE_FOR_FILENAME}.map)
-   set(eeprom_image ${EXECUTABLE_NAME}${MCU_TYPE_FOR_FILENAME}-eeprom.hex)
 
    # elf file
    add_executable(${elf_file} EXCLUDE_FROM_ALL ${ARGN})
@@ -163,7 +164,7 @@ function(add_avr_executable EXECUTABLE_NAME)
       ${elf_file}
       PROPERTIES
          COMPILE_FLAGS "-mmcu=${AVR_MCU}"
-         LINK_FLAGS "-mmcu=${AVR_MCU} -Wl,--gc-sections -mrelax -Wl,-Map,${map_file}"
+         LINK_FLAGS "-mmcu=${AVR_MCU} -Wl,--gc-sections -mrelax -Wl,-Map,${map_file} -nostartfiles"
    )
 
    add_custom_command(
@@ -175,20 +176,10 @@ function(add_avr_executable EXECUTABLE_NAME)
       DEPENDS ${elf_file}
    )
 
-   # eeprom
-   add_custom_command(
-      OUTPUT ${eeprom_image}
-      COMMAND
-         ${AVR_OBJCOPY} -j .eeprom --set-section-flags=.eeprom=alloc,load
-            --change-section-lma .eeprom=0 --no-change-warnings
-            -O ihex ${elf_file} ${eeprom_image}
-      DEPENDS ${elf_file}
-   )
-
    add_custom_target(
       ${EXECUTABLE_NAME}
       ALL
-      DEPENDS ${hex_file} ${eeprom_image}
+      DEPENDS ${hex_file}
    )
 
    set_target_properties(
@@ -202,68 +193,6 @@ function(add_avr_executable EXECUTABLE_NAME)
    set_directory_properties(
       PROPERTIES
          ADDITIONAL_MAKE_CLEAN_FILES "${map_file}"
-   )
-
-   # upload - with avrdude
-   add_custom_target(
-      upload_${EXECUTABLE_NAME}
-      ${AVR_UPLOADTOOL} -p ${AVR_MCU} -c ${AVR_PROGRAMMER} ${AVR_UPLOADTOOL_OPTIONS}
-         -U flash:w:${hex_file}
-         -P ${AVR_UPLOADTOOL_PORT}
-      DEPENDS ${hex_file}
-      COMMENT "Uploading ${hex_file} to ${AVR_MCU} using ${AVR_PROGRAMMER}"
-   )
-
-   # upload eeprom only - with avrdude
-   # see also bug http://savannah.nongnu.org/bugs/?40142
-   add_custom_target(
-      upload_eeprom
-      ${AVR_UPLOADTOOL} -p ${AVR_MCU} -c ${AVR_PROGRAMMER} ${AVR_UPLOADTOOL_OPTIONS}
-         -U eeprom:w:${eeprom_image}
-         -P ${AVR_UPLOADTOOL_PORT}
-      DEPENDS ${eeprom_image}
-      COMMENT "Uploading ${eeprom_image} to ${AVR_MCU} using ${AVR_PROGRAMMER}"
-   )
-
-   # get status
-   add_custom_target(
-      get_status
-      ${AVR_UPLOADTOOL} -p ${AVR_MCU} -c ${AVR_PROGRAMMER} -P ${AVR_UPLOADTOOL_PORT} -n -v
-      COMMENT "Get status from ${AVR_MCU}"
-   )
-
-   # get fuses
-   add_custom_target(
-      get_fuses
-      ${AVR_UPLOADTOOL} -p ${AVR_MCU} -c ${AVR_PROGRAMMER} -P ${AVR_UPLOADTOOL_PORT} -n
-         -U lfuse:r:-:b
-         -U hfuse:r:-:b
-      COMMENT "Get fuses from ${AVR_MCU}"
-   )
-
-   # set fuses
-   add_custom_target(
-      set_fuses
-      ${AVR_UPLOADTOOL} -p ${AVR_MCU} -c ${AVR_PROGRAMMER} -P ${AVR_UPLOADTOOL_PORT}
-         -U lfuse:w:${AVR_L_FUSE}:m
-         -U hfuse:w:${AVR_H_FUSE}:m
-         COMMENT "Setup: High Fuse: ${AVR_H_FUSE} Low Fuse: ${AVR_L_FUSE}"
-   )
-
-   # get oscillator calibration
-   add_custom_target(
-      get_calibration
-         ${AVR_UPLOADTOOL} -p ${AVR_MCU} -c ${AVR_PROGRAMMER} -P ${AVR_UPLOADTOOL_PORT}
-         -U calibration:r:${AVR_MCU}_calib.tmp:r
-         COMMENT "Write calibration status of internal oscillator to ${AVR_MCU}_calib.tmp."
-   )
-
-   # set oscillator calibration
-   add_custom_target(
-      set_calibration
-      ${AVR_UPLOADTOOL} -p ${AVR_MCU} -c ${AVR_PROGRAMMER} -P ${AVR_UPLOADTOOL_PORT}
-         -U calibration:w:${AVR_MCU}_calib.hex
-         COMMENT "Program calibration status of internal oscillator from ${AVR_MCU}_calib.hex."
    )
 
    # disassemble
